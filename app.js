d030465e661111b7699474c1b047629f8f8b42b4
sharooq15 <sharooqannauniv.ceg@gmail.com>
2021-04-27 19:15:54 +0530

    Add express js and add basic code

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
+// Node specific modules
 const http = require('http');
 
-const routes = require('./routes');
+// Third party imports
+const express = require('express');
 
-console.log(routes.someText);
+// Self imports
 
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(3000);
+// Using middle wares
+// The functions added to it will be executed for each incoming request. 
+app.use((req, res, next) => {
+  console.log('inside the middleware');
+  next(); // This allows the middleware to go to the next functions.
+});
+
+app.use((req, res, next) => {
+  console.log('from other middleware');
+  // There is no default response.
+  res.send('<h1>Hello from express js</h1>');
+})
+
+// This creates a server and listens to the particular port.
+app.listen(3000);
