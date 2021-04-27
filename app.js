6cec5bfc8de52b17eca6e775fc7738dfc6e14a91
sharooq15 <sharooqannauniv.ceg@gmail.com>
2021-04-27 20:12:52 +0530

    Add code for parsing body from request

--- app.js
+++ app.js
@@ -3,23 +3,31 @@ const http = require('http');
 
 // Third party imports
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // Self imports
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
 // Using middle wares
 // The functions added to it will be executed for each incoming request. 
-app.use((req, res, next) => {
-  console.log('inside the middleware');
-  next(); // This allows the middleware to go to the next functions.
+// app.use('/add-product',(req, res, next) => {
+//   console.log('inside the middleware');
+//   next(); // This allows the middleware to go to the next functions.
+// });
+
+app.use('/add-product',(req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 });
 
-app.use((req, res, next) => {
-  console.log('from other middleware');
-  // There is no default response.
-  res.send('<h1>Hello from express js</h1>');
+// Filters only the post requests.
+app.post('/product',(req, res, next) => {
+  console.log('req', req.body);
+  res.redirect('/');
 })
-
+app.use('/',(req, res, next) => {
+  res.send('helo for root')
+})
 // This creates a server and listens to the particular port.
 app.listen(3000);
