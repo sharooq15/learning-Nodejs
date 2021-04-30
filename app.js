bdb5e67c0bcdbf8f509d8e657873a5780277c9fc
sharooq15 <sharooqannauniv.ceg@gmail.com>
2021-04-30 19:27:51 +0530

    Add 404 screen

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 // Self imports
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(adminRouter);
+app.use(shopRouter);
+app.use((req,res,next) => {
+  res.status(404).send('404 Not Found');
+})
 // Using middle wares
 // The functions added to it will be executed for each incoming request. 
 // app.use('/add-product',(req, res, next) => {
@@ -17,17 +24,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 //   next(); // This allows the middleware to go to the next functions.
 // });
 
-app.use('/add-product',(req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-});
 
-// Filters only the post requests.
-app.post('/product',(req, res, next) => {
-  console.log('req', req.body);
-  res.redirect('/');
-})
-app.use('/',(req, res, next) => {
-  res.send('helo for root')
-})
+
 // This creates a server and listens to the particular port.
 app.listen(3000);
